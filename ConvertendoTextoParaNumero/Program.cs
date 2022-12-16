3f8b58d12e4f7539eb45113d133a0e6dd14e61ce
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-12-15 22:37:05 -0300

    Adicionar arquivos de projeto.

---
+++ ConvertendoTextoParaNumero/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            string NumeroTexto = "30";
+
+            int Numero1 =  int.Parse(NumeroTexto);
+            int Numero2 = 30;
+
+            Console.WriteLine("O resultado da soma é: " + (Numero1 + Numero2));
+            /*Precisa colocar entre ( ) para existir a soma */
+
+
+        }
+    }
+}
